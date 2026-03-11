088e3365f8e67635381bc94e4bc0add47adf856b
shmu1k4OO <149907485+shmu1k4OO@users.noreply.github.com>
2026-03-11 19:09:43 +0300

    feat: add templates

--- main.py
+++ main.py
@@ -1,4 +1,79 @@
+import datetime as _dt
+import itertools as _it
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+# =============================== КОНСТАНТЫ ===============================
+DEFAULT_CARD_INFO_FIELDS = [
+    "card_id",
+    "user_id",
+    "phone",
+    "bank_name",
+    "bank_bic",
+    "acc_id",
+    "pan",
+    "payment_system",
+    "currency",
+    "status",
+    "issue_date",
+    "expiry_date",
+    "balance",
+    "cashback_balance",
+    "user_cards",
+]
+DEFAULT_ACCOUNT_BALANCE = 0.00
+DEFAULT_CASHBACK_BALANCE = 0.00
+CARD_CURRENCY = "RUB"
+DEFAULT_PAYMENT_SYSTEM = "MIR"
+
+ACCOUNT_TYPE_CODE = "40817"  # тип счета для физлиц
+ACCOUNT_BRANCH = "0000"  # отсутствие филиалов у банка
+ACCOUNT_CURRENCY = "810"  # идентификатор для рублёвых операций
+
+
+EMPTY_PAN = "0000000000000000"
+
+BIN_BY_SYSTEM = {
+    "MIR": "220400",
+    "VISA": "400000",
+    "MASTERCARD": "510000",
+}
+
+
+# =============================== ГЕНЕРАТОРЫ ДАННЫХ ===============================
+ISSUE_DATE_START = _dt.date(2022, 1, 1)
+ISSUE_DATE_GENERATOR = (ISSUE_DATE_START + _dt.timedelta(days=i) for i in _it.count())
+EXPIRY_YEARS = 4
+
+
+# =============================== ENUM'Ы ===============================
+class CardStatus(Enum):
+    ACTIVE = "Active"
+    CLOSED = "Closed"
+    BLOCKED = "Blocked"
+
+
+CARD_STATUS = CardStatus.ACTIVE
+
+
+@dataclass
 class Bank:
+
+    name: str
+    bic: str
+
+    _user_seq: any = field(default_factory=lambda: _it.count(1), init=False)
+    _account_seq: any = field(default_factory=lambda: _it.count(1), init=False)
+    _card_seq: any = field(default_factory=lambda: _it.count(1), init=False)
+    _pan_seq: any = field(default_factory=lambda: _it.count(1), init=False)
+
+    def _next_account_number(self):
+         
+         mask = [7, 1, 3] * 8
+
+         
+
 
     def checker(self, serial_number, key, flag="CREDIT_ORG", bic="044525225", val="810", type_of_number="40817"):
         mask = "71371371371371371371371"
@@ -31,10 +106,5 @@ class Bank:
                 return control_key
         else:
              raise("Ошибка подбора ключа!")
-        
-        
-
-repository = Bank()
-repository_1 = Bank()
-print(repository.checker("0000025", "0", bic="049805746", val="810", type_of_number="40602"))
+    
 
