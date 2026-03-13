37f2957d37ebbff7e5b9269b29ef6b898bfb5d17
shmu1k4OO <149907485+shmu1k4OO@users.noreply.github.com>
2026-03-13 14:54:39 +0300

    feat: new two methods and classes

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ BIN_BY_SYSTEM = {
 
 
 # =============================== ГЕНЕРАТОРЫ ДАННЫХ ===============================
-ISSUE_DATE_START = _dt.date(2022, 1, 1)
+ISSUE_DATE_START = _dt.date(2026, 1, 1)
 ISSUE_DATE_GENERATOR = (ISSUE_DATE_START + _dt.timedelta(days=i) for i in _it.count())
 EXPIRY_YEARS = 4
 
@@ -58,10 +58,68 @@ CARD_STATUS = CardStatus.ACTIVE
 
 
 @dataclass
+class User:
+    last_name: str
+    first_name: str
+    phone: str
+    pin: str
+    user_id: int
+
+    accounts: list = field(default_factory=list)
+    cards: list = field(default_factory=list)
+
+
+@dataclass
+class Account:
+    owner: "User"
+    acc_id: str
+    balance: float = DEFAULT_ACCOUNT_BALANCE
+
+
+class Card:
+    def __init__(
+            self,
+            account,
+            card_id,
+            payment_system,
+            pan,
+            issue_date,
+            expiry_date,
+            status=CARD_STATUS,
+            card_currency=CARD_CURRENCY,
+            bank_name=None,
+
+
+    ):  
+        self.account = account
+        self.card_id = card_id
+        self.payment_system = payment_system
+        self.pan = pan
+        self.issue_date = issue_date
+        self.expiry_date = expiry_date
+        self.status = status
+        self.card_currency = card_currency
+        self.bank_name = bank_name
+
+        if self.issue_date is None:
+            self.issue_date = next(ISSUE_DATE_GENERATOR)
+        if self.expiry_date is None and self.issue_date is not None:
+            self.expiry_date = _dt.date(
+                self.issue_date.year + EXPIRY_YEARS,
+                self.issue_date.month,
+                self.issue_date.day,
+            )
+        get_card_info(self,):
+
+
+@dataclass
 class Bank:
 
     name: str
     bic: str
+    customers: list = field(default_factory=list)
+    accounts: list = field(default_factory=list)
+    cards: list = field(default_factory=list)
 
     _user_seq: any = field(default_factory=lambda: _it.count(1), init=False)
     _account_seq: any = field(default_factory=lambda: _it.count(1), init=False)
@@ -107,4 +165,56 @@ class Bank:
             else:
                 digits[i] = double_value_of_digit
         
-        return (10 - sum(digits) % 10) % 10
+        return (10 - sum(digits) % 10) % 1
+    
+    def aplly_for_card(
+            self,
+            last_name,
+            first_name,
+            pin,
+            phone,
+            payment_system=DEFAULT_PAYMENT_SYSTEM,
+            card_class: type = Card,
+            **kwargs,
+    ):  
+        pan = self._generate_pan(payment_system)
+
+        account = self._next_account_number()
+
+        card = Card(account,
+                    self._card_seq,
+                    payment_system, 
+                    pan, 
+                    issue_date=None, 
+                    expiry_date=None, 
+                    status=CARD_STATUS, 
+                    card_currency=CARD_CURRENCY, 
+                    bank_name=self.name
+                    )
+        
+        if phone not in [user.phone for user in self.customers] and last_name not in [user.last_name for user in self.customers] and first_name not in [user.first_name for user in self.customers]:
+            
+            user = User(last_name,
+                        first_name,
+                        phone, 
+                        pin, 
+                        next(self._user_seq), 
+                        accounts=[account], 
+                        cards=[card])
+        
+            self.customers.append(user)
+            self.accounts.append(account)
+            self.cards.append(card)
+
+        else:
+             for user in self.customers:
+                if user.phone == phone or user.last_name == last_name or user.first_name == first_name:
+                    user.accounts.append(account)
+                    user.cards.append(card)
+                    break
+
+             self.accounts.append(account)
+             self.cards.append(card)
+                
+        return card
+    
