c92963c1cdf3241ad0936c7d4239ab3a068f5d46
shmu1k4OO <149907485+shmu1k4OO@users.noreply.github.com>
2026-06-04 21:46:22 +0300

    Revert "feat: 5.5 OK, exceptions are working"
    This reverts commit 58c8c7a3a35ee919bbfa39048b8cbbd456387617.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ DEFAULT_CASHBACK_BALANCE = 0.00
 DEFAULT_CASHBACK_TRANSACTION = 0.00
 CARD_CURRENCY = "RUB"
 DEFAULT_PAYMENT_SYSTEM = "MIR"
-FORBIDDEN_MCC = {"7995", "7996", "7997", "7998", "7999", "4829"}  # код категории продавца для азартных игр и лотерей
+FORBIDDEN_MCC = {"7995", "7996", "7997", "7998", "7999"}  # код категории продавца для азартных игр и лотерей
 
 ACCOUNT_TYPE_CODE = "40817"  # тип счета для физлиц
 ACCOUNT_BRANCH = "0000"  # отсутствие филиалов у банка
@@ -153,15 +153,6 @@ class InsufficientFundsError(BankError):
     INSUFFICIENT_FUNDS_FOR_TRANSFER = "Недостаточно денег для осуществления перевода."
 
 
-def handle_errors(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except (ValidationError, AccessError, NotFoundError, BusinessRuleError, InsufficientFundsError) as e:
-            print(e)
-    return wrapper
-
-
 # =============================== ENUM'Ы ===============================
 class CardStatus(Enum):
     ACTIVE = "Active"
@@ -189,7 +180,7 @@ class User:
 
     accounts: list = field(default_factory=list)
     cards: list = field(default_factory=list)
-    @handle_errors
+
     def change_pin(self, old_pin, new_pin):
         if (
             not (isinstance(new_pin, str) and len(str(new_pin)) == 4)
@@ -204,7 +195,7 @@ class User:
         elif self.pin != old_pin:
             raise ValidationError(ValidationError.PIN_MISMATCH)
         else:
-            self.pin = new_pin
+            self.pin = new_pin 
 
 
 @dataclass
@@ -253,8 +244,7 @@ class Card:
                 self.issue_date.day,
             )
 
-    @handle_errors
-    def get_card_info(self, fields: list = None):    
+    def get_card_info(self, fields: list = None):
         if self.account is None or self.account.owner is None:
             raise AccessError(AccessError.ACCOUNT_NOT_LINKED)
         if self.status == CardStatus.CLOSED or self.status == CardStatus.BLOCKED:
@@ -285,7 +275,6 @@ class Card:
             + "\n"
             + "-" * 50
         )
-        
 
     def __repr__(self):
         return (
@@ -293,7 +282,6 @@ class Card:
             f"status={self.status}, issue_date={self.issue_date}, expiry_date={self.expiry_date})"
         )
     
-    @handle_errors
     def get_balance(self):
         if self.account is None:
             raise AccessError(AccessError.ACCOUNT_NOT_LINKED)
@@ -301,12 +289,11 @@ class Card:
             raise AccessError(AccessError.CARD_CLOSED)
         else:
             return f"Баланс: {self.account.balance:.2f}₽"
-        
+
     def close(self):
         self.status = CardStatus.CLOSED
 
-    @handle_errors
-    def deposit(self, amount):    
+    def deposit(self, amount):
         if amount <= 0:
             raise ValidationError(ValidationError.DEPOSIT_AMOUNT_NEGATIVE)
         if self.account is None:
@@ -331,9 +318,8 @@ class Card:
             timestamp  
         )
         self.bank.transaction_log.append(transaction)
-        
-    @handle_errors    
-    def transfer(self, to_card, amount):     
+            
+    def transfer(self, to_card, amount):
         if amount <= 0:
             raise ValidationError(ValidationError.AMOUNT_NEGATIVE)
         if to_card is None:
@@ -374,8 +360,7 @@ class Card:
         )
         self.bank.transaction_log.append(transaction)
             
-    @handle_errors        
-    def pay(self, amount, mcc):     
+    def pay(self, amount, mcc):
         if amount <= 0:
             raise ValidationError(ValidationError.PAY_AMOUNT_NEGATIVE)
         elif not re.fullmatch(r"\d{4}", mcc):
@@ -406,8 +391,7 @@ class Card:
                 timestamp
             )
             self.bank.transaction_log.append(transaction)
-    
-    @handle_errors
+            
     def get_transaction_history(self):
         if self.account is None or self.account.owner is None:
             raise AccessError(AccessError.ACCOUNT_NOT_LINKED)
@@ -436,7 +420,7 @@ class Card:
                     f"{transaction.cashback:.2f}₽,{transaction.description}"
                 )
         return card_transaction_history
-        
+
    
 @dataclass
 class Transaction:
@@ -505,7 +489,6 @@ class Bank:
         
         return (10 - sum(digits) % 10) % 10
     
-    @handle_errors
     def apply_for_card(
             self,
             last_name,
@@ -686,7 +669,6 @@ class CashbackDebitCard(Card):
         )
         self.cashback_rate = cashback_rate
 
-    @handle_errors
     def pay(self, amount, mcc):
         if amount <= 0:
             raise ValidationError(ValidationError.PAY_AMOUNT_NEGATIVE)
@@ -767,8 +749,7 @@ class SavingCard(Card):
         )
         self.interest_rate = interest_rate
     
-    @handle_errors
-    def accrue_interest(self): 
+    def accrue_interest(self):
         if self.interest_rate < 0:
             raise ValidationError(ValidationError.INTEREST_NEGATIVE)
         elif self.account is None:
@@ -794,18 +775,29 @@ class SavingCard(Card):
                 timestamp
             )
             self.bank.transaction_log.append(transaction)
-        
-    @handle_errors
-    def pay(self, amount, mcc):
-            raise BusinessRuleError(BusinessRuleError.PAYMENT_NOT_ALLOWED_FOR_SAVING)
     
+    def pay(self, amount, mcc):
+        raise BusinessRuleError(BusinessRuleError.PAYMENT_NOT_ALLOWED_FOR_SAVING)
+    
+bank = Bank("Тестовый Банк", "123456789")
+user = User("Иванов", "Иван", "89001234567", "1234", 1)
+user_2 = User("Петров", "Пётр", "89007654321", "4321", 2)
+card = bank.apply_for_card(
+    last_name=user.last_name,
+    first_name=user.first_name,
+    phone=user.phone,
+    pin=user.pin
+)
 
-bank = Bank('Demo Bank', '044452345')
-c1 = bank.issue_simple_debit_card("Иванов", "Иван", "1234", "+70000000001", "MIR")
-c1.deposit(100)
-c1.pay(50, "5411")
-c1.pay(70, "4829")
-c1.pay(500005.0, "5411")
-c1.pay(10, "5411")
-c1.account.owner.change_pin("1234", "12")
-c1.account.owner.change_pin("1234", "0000")
+card_1 = bank.apply_for_card(
+    last_name=user_2.last_name,
+    first_name=user_2.first_name,
+    phone=user_2.phone,
+    pin=user_2.pin
+)
+
+card.deposit(100000)
+card.transfer(None, 1000)
+
+print(card_1.get_transaction_history())
+print(card_1.get_card_info())
